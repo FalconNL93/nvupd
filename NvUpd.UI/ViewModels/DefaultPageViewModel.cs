ea8c6e69a8c99e2175919e0d3d96ec004815a549
Peter van der Veen <p.vanderveen93@gmail.com>
2023-06-26 08:04:09 +0200

    Add default page

---
+++ NvUpd.UI/ViewModels/DefaultPageViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Nvupd.Core.Models;
+using NvUpd.UI.Models;
+using NvUpd.UI.Services;
+
+namespace NvUpd.UI.ViewModels;
+
+public partial class DefaultPageViewModel : ObservableRecipient
+{
+    private readonly GpuService _gpuService;
+
+    [ObservableProperty]
+    private GpuInformation _gpuInformation = new();
+
+    [ObservableProperty]
+    private NvidiaUpdate _nvidiaUpdate = new();
+
+    public DefaultPageViewModel(GpuService gpuService)
+    {
+        _gpuService = gpuService;
+    }
+
+    [RelayCommand]
+    private void OpenUrl(Uri downloadUri)
+    {
+        Process.Start(new ProcessStartInfo
+        {
+            UseShellExecute = true,
+            FileName = downloadUri.ToString()
+        });
+    }
+
+    public async Task CheckForUpdates()
+    {
+        NvidiaUpdate = await _gpuService.GetLatestUpdates();
+    }
+}
---
+++ NvUpd.UI/Views/Pages/DefaultPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+using NvUpd.UI.ViewModels;
+
+namespace NvUpd.UI.Views.Pages;
+
+public partial class DefaultPage : Page
+{
+    public DefaultPage(DefaultPageViewModel viewModel)
+    {
+        ViewModel = viewModel;
+        
+        InitializeComponent();
+    }
+    
+    public DefaultPageViewModel ViewModel { get; }
+}
